39126f61682f04dd1ad91bef0d8d3abfaebc3f1a
ARBAZ KHAN <khanarbaz0674@gmail.com>
2023-05-19 01:06:56 +0530

    Worked on Alert Component and Introduced Edit and Delete Icon

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -8,6 +8,14 @@ export default function NoteItem(props) {
         <div className="card-body">
           <h5 className="card-title">{note.title}</h5>
           <p className="card-text">{note.description}</p>
+          <i
+            className="fa-solid fa-pen-to-square mx-2"
+            style={{ color: "#13c6c9" }}
+          ></i>
+          <i
+            className="fa-solid fa-trash mx-2"
+            style={{ color: "#1f5151" }}
+          ></i>
         </div>
       </div>
     </div>
